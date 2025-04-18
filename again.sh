074488a9b7e6c06cd0dc5ef179a4fc795f4fa3d8
fevunge <developervunge@gmail.com>
2025-04-18 14:56:12 +0000

    MELHORA, E AGORA NAO PARA EXECUCA PELO COMANDO <ssh-keygen>

--- again.sh
+++ again.sh
@@ -4,48 +4,75 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 
 nvm install 22
 
-echo "Node " $(node -v) " ,foi instalado"
-echo "NVM " $(nvm current) " ,foi instalado"
-echo "NPM " $(npm -v ) " ,foi instalado"
+echo "+------------------------------------+"
+echo "| Node" $(node -v)", FOI INSTALADO   |"
+echo "| NVM" $(nvm current)", FOI INSTALADO|"
+echo "| NPM" $(npm -v )", FOI INSTALADO    |"
+echo "+------------------------------------+"
 
 npm install -g npm
-echo "Node Package Manager foi actualizado"
+echo "+------------------------------------+"
+echo "|NODE PACKAGE MANAGER FOI ACTUALIZADO|"
+echo "+------------------------------------+"
 
-ssh-keygen -t ed25519 -C "I Love Archcraft"
+
+ssh-keygen -t ed25519 -C "I Love Archcraft" -f ~/.ssh/id_ed25519 -N ""
 cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
 
-echo "Abra o Firefox (Ctrl + W)!"
-echo "Quando Fizer isso digite sim([sim] ja fiz | [nao], vou fazer depois)"
+echo "+-------------------------------------------------+"
+echo "|CHAVE GERADA E COPIADA PARA AREA DE TRANFERENCIA |"
+echo "+-------------------------------------------------+"
 
-read resposta
+echo -e "\t>> ABRA O FIREFOX (Ctrl + W)!\t\t[<enter>]"
+read
 
-if [[ "$resposta" == "sim" || "$resposta" == "s" ]]; then
-    echo "Você escolheu sim! Vou abrir o GitHub Agora"
-    firefox github.com/login
-fi
+echo
 
-echo "Ja fez o login?[sim]"
-read resposta
+echo "+-----------------------------------------------------+"
+echo "| VOCE ME COMFIRMOU QUE O FIREFOX ESTA ABERTO         |"
+echo "| ENTRE NA SUA CONTA, ME AVISE QUANDO FIZER ISSO      |"
+echo "+-----------------------------------------------------+"
+firefox github.com/login
+echo -e "\t>>VOCE JA ESTA LOGADO?\t\t[<enter>]"
+read
 
-if [[ "$resposta" == "sim" || "$resposta" == "s"]]; then
-    echo "Vou copiar a chave SSH na area de tranferencia agora"
-    cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
-    echo "Feito!"
-fi
+echo
+firefox github.com/settings/keys
 
-echo "Quando inserio a chave na sua conta github, me avise[ja]"
-read resposta
+cat ~/.ssh/id_ed25519.pub | xclip -selection clipboard
+echo "+------------------------------------------------------------------+"
+echo "| COLE A CHAVE SSH EM [New SSH key]                                |"
+echo "| ME AVISE QUANDO TERMINAR, NAO ESQUECA DE ELIMINAR A CHAVE ANTIGA |"
+echo "+------------------------------------------------------------------+"
 
-if [[ "$resposta" == "ja" | "$respota" =="j" ]]; then
-    echo "Muito bem, vou prepara eu ambiente de trabalho agora"
-    echo "PROJECTO MIAU ANGOWAY!"
-    git clone git@github.com:fevunge/miau-angoway.git
-    cd miau-angoway
-    npm install
-fi
+echo -e "\t>> VOCE JA CRIOU A NOVA CHAVE SSH E DELETOU A ANTIGA?\t\t[<enter>]"
+read
 
+echo
+echo "+-----------------------------------------------------------------+"
+echo "| MUITO BEM, AGORA ESPERE QUE EU PREPARO SEU AMBIENTE DE TRABALHO |"
+echo "| * VOU BAIXAR O PROJECTO!                                        |"
+echo "| * E INSTALAR AS DEPENDECIAS                                     |"
+echo "+-----------------------------------------------------------------+"
+
+git clone git@github.com:fevunge/miau-angoway.git
+cd miau-angoway
+npm install
 git config --global user.name "fevunge"
 git config --global user.email "developervunge@gmail.com"
 
-echo "Feito"
-echo "Bom trabalho!"
+echo "+-------------------------------------------------+"
+echo "| FEITO, TENHA UMA BOA PROGRAMACAO                |"
+echo "| GOSTARIA DE FAZER ALGUMA ACTUALIZACAO NO AGAIN? |"
+echo "+-------------------------------------------------+"
+
+echo -e "\t>>SE SIM, ME DIGA\t\t[<sim>]"
+read se_sim
+
+echo
+if  [[ "$se_sim" == "sim" ]]; then
+    cd
+    git clone git@github.com:fevunge/livefevunge.git
+    cd livefevunge/
+    vim again.sh
+fi
